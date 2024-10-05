a58ca8cdbc6d63f6e8f08eecd44861a1c4379ea6
solo-granule0c <solo-granule0c@icloud.com>
2024-10-05 14:24:09 +0300

    Updated resources

--- resources/database.sql
+++ resources/database.sql
@@ -6,3 +6,4 @@ Data Science Deployment Do what you love and the money will follow. Performance 
 The only way to do great work is to love what you do. DevOps Algorithm Virtual Machines Happiness is not something ready made. It comes from your own actions. Scalability Stay hungry, stay foolish. Kafka
 Don't be afraid to give up the good to go for the great. Python Data Science Artificial Intelligence DevOps Docker Frontend A/B Testing
 Database Multi-threading Machine Learning Event Sourcing Replica Set Dream big and dare to fail. Deployment Opportunities don't happen. You create them. Backend Firewalls Zero Downtime Deployment Testing Scalability
+Cloud Logging Event-driven Architecture Success doesn’t just find you. You have to go out and get it. Event Sourcing Work hard in silence, let your success be your noise. Cache Invalidation
