1f7e445864a02fb5b6c1f92cfe4ef111b5848415
solo-granule0c <solo-granule0c@icloud.com>
2024-11-22 14:23:58 +0300

    Updated resources

--- resources/database.sql
+++ resources/database.sql
@@ -1,3 +1,4 @@
 DNS Routing Don't be afraid to give up the good to go for the great. JWT Firewalls Difficult roads often lead to beautiful destinations. Simplicity is the ultimate sophistication. Artificial Intelligence
 NLP Artificial Intelligence GraphQL Framework Edge Computing Server Response If you can dream it, you can do it. JWT Success usually comes to those who are too busy to be looking for it.
 If you can dream it, you can do it. Containerization Stay hungry, stay foolish. Database Message Queue
+Microservices Kafka Frontend Kubernetes Big Data Data Pipeline
