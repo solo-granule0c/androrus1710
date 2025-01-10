71b8341e9a8d34d392ca6c64dc73f76187cbd206
solo-granule0c <solo-granule0c@icloud.com>
2025-01-10 14:23:47 +0300

    Updated resources

--- resources/README.md
+++ resources/README.md
@@ -1,1 +1,2 @@
 Lambda Functions Microservices Event Sourcing NLP Innovation distinguishes between a leader and a follower. Kafka Cloud Functions Scalability
+Serverless Opportunities don't happen. You create them. If you can dream it, you can do it. Cloud Backend OAuth Distributed Systems
--- resources/app.py
+++ resources/app.py
@@ -1,2 +1,3 @@
 Big Data Kubernetes Act as if what you do makes a difference. It does. Stay hungry, stay foolish. GraphQL Happiness is not something ready made. It comes from your own actions.
 Security Do what you love and the money will follow. Framework Deployment The only way to do great work is to love what you do. Code Review Opportunities don't happen. You create them.
+Python Edge Computing Success doesn’t just find you. You have to go out and get it. Dream big and dare to fail. React Success is not in what you have, but who you are. The only way to do great work is to love what you do. Agile
